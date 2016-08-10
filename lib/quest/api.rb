a0b27ed0763c1e3461679ecb6dbf4c0a3371021a
kjhenner <kjhenner@gmail.com>
2016-08-10 10:47:42 -0700

    update api

--- lib/quest/api.rb
+++ lib/quest/api.rb
@@ -1,10 +1,15 @@
 require 'sinatra/base'
 
 module Quest
+
   class API < Sinatra::Base
 
     get '/status' do
-      settings.messenger.quest_status[settings.messenger.active_quest]
+      settings.messenger.quest_status[settings.mesisenger.active_quest].to_json
+    end
+
+    get '/active_quest' do
+      settings.messenger.active_quest
     end
 
     get '/quests' do
@@ -12,7 +17,7 @@ module Quest
     end
 
     post '/begin/:quest' do
-      settings.messenger.begin_quest([:quest])
+      settings.messenger.begin_quest(params[:quest])
     end
   end
 
