7ae960c444c7d6737e70db9510fba1139499bad5
kjhenner <kjhenner@gmail.com>
2016-08-10 16:41:16 -0700

    elaborate api endpoints

--- lib/quest/api.rb
+++ lib/quest/api.rb
@@ -4,20 +4,67 @@ module Quest
 
   class API < Sinatra::Base
 
+    helpers do
+      def messenger
+        settings.messenger
+      end
+      def quest_status
+        settings.messenger.quest_status
+      end
+      def active_quest
+        settings.messenger.active_quest
+      end
+      def active_quest_status
+        settings.messenger.quest_status[settings.messenger.active_quest]
+      end
+    end
+
     get '/status' do
-      settings.messenger.quest_status[settings.messenger.active_quest].to_json
+      active_quest_status.to_json
+    end
+
+    get '/status/examples' do
+      active_quest_status[examples].to_json
+    end
+
+    get '/status/examples/:number' do
+      active_quest_status[examples][params[:number].to_i - 1].to_json
+    end
+
+    get '/status/examples/count' do
+      active_quest_status[examples].size
+    end
+
+    get '/status/examples/:number/description' do
+      active_quest_status[examples][params[:number].to_i - 1]["description"].to_json
+    end
+
+    get '/status/examples/:number/file_path' do
+      active_quest_status[examples][params[:number].to_i - 1]["file_path"].to_json
+    end
+
+    get '/status/examples/:number/status' do
+     active_quest_status[examples][params[:number].to_i - 1]["status"].to_json
+    end
+
+    get '/status/examples/:number/run_time' do
+      active_quest_status[examples][params[:number].to_i - 1]["run_time"].to_json
+    end
+
+    get '/status/summary' do
+      active_quest_status["summary"] 
     end
 
     get '/active_quest' do
-      settings.messenger.active_quest
+      active_quest
     end
 
     get '/quests' do
-      settings.messenger.quests.to_json
+      messenger.quests.to_json
     end
 
     post '/begin/:quest' do
-      settings.messenger.begin_quest(params[:quest])
+      messenger.begin_quest(params[:quest])
     end
   end
 
